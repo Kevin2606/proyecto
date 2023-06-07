47f6334ccd3248d05963b3ca22f6978ff4f48308
Kevin G <andreskevin2606@gmail.com>
2023-06-07 17:31:34 -0500

    feat: :rocket: Primer Commit
    Primer commit con la maquetacion del proyecto crud

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css" />
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <div id="container">
+            <div>
+                <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+                <input type="text" name="apellido" id="apellido" placeholder="Apellido">
+                <input type="text" name="direccion" id="direccion" placeholder="Direccion">
+            </div>
+            <div>
+                <p>Campuslands</p>
+                <input type="text" name="edad" id="edad" placeholder="Edad">
+                <input type="email" name="email" id="email" placeholder="usuario@correo.com">
+            </div>
+            <div>
+                <input type="text" name="horario" id="horario" placeholder="Horario de entrada">
+                <input type="text" name="team" id="team" placeholder="Team">
+                <input type="text" name="trainer" id="trainer" placeholder="Trainer">
+            </div>
+            <div id="acciones">
+                <input type="submit" value="Enviar">
+                <input type="submit" value="Editar">
+                <input type="submit" value="Eliminar">
+                <input type="submit" value="Buscar">
+                <input type="number" name="cedula" id="cedula" placeholder="Cedula">
+            </div>
+        </div>
+    </form>
+</body>
+</html>
